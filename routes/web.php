f3528fe9869fe5edbcfd3729f0a681f69079ebd8
Kauã <kauavmm@hotmail.com>
2026-07-26 23:36:13 +0100

    Controller created with 'php artisan make:controller' and using the MVC structure

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\ChirpController;
 use Illuminate\Support\Facades\Route;
 
+/* 
+It will be the one at the bottom
 Route::get('/', function () {
     return view('home');
-});
+}); 
+*/
+
+Route::get('/', [ChirpController::class, 'index']);
